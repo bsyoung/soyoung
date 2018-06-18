37054e026a9878d122a3286ed0af137193f0667e
bsy <3_aaa_3@naver.com>
2018-06-18 11:09:52 +0900

    add master

--- test/src/test/helloword.java
+++ test/src/test/helloword.java
@@ -5,6 +5,7 @@ public class helloword {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello~~~~start");
+		System.out.println("hello~~~~mid");
 		System.out.println("hello~~~~end");
 	}
 
