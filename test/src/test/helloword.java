49d46e966e84d1d926bbb9741e46ac51b6d3da37
bsy <3_aaa_3@naver.com>
2018-06-18 13:50:18 +0900

    zzzzzzzzzzzzzz

--- test/src/test/helloword.java
+++ test/src/test/helloword.java
@@ -7,6 +7,9 @@ public class helloword {
 		System.out.println("hello~~~~start");
 		System.out.println("hello~~~~mid");
 		System.out.println("hello~~~~end");
+		System.out.println("hello~~~~end");
+		System.out.println("hello~~~~end");
+		System.out.println("hello~~~~end");
 	}
 
 }
