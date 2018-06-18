1d4a21d264aa0a5dc8ba5dd7af0b85fdd33a6fb2
bsy <3_aaa_3@naver.com>
2018-06-18 11:07:23 +0900

    added print line

--- test/src/test/helloword.java
+++ test/src/test/helloword.java
@@ -4,7 +4,8 @@ public class helloword {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello~~~~");
+		System.out.println("hello~~~~start");
+		System.out.println("hello~~~~end");
 	}
 
 }
